608ea865ea41d2ceaa7b5ab7bb7dded7bf1cb120
Robin Ghyselinck <rghyselinck@gmail.com>
2023-09-14 09:33:52 +0200

    adapted script to handle multiple image source folders

--- scripts/serve_local_files.sh
+++ scripts/serve_local_files.sh
@@ -1,31 +1,38 @@
 #!/usr/bin/env bash
-INPUT_DIR=$1
-WILDCARD=${2}
-OUTPUT_FILE=${3:-"files.txt"}
-PORT=${4:-3000}
 
-echo "Usage: sh serve_local_files.sh INPUT_DIR WILDCARD OUTPUT_FILE PORT"
+# Get the arguments from command line
+ROOT_DIR=$1
+WILDCARD=${2:-"*"}
+PORT=${3:-3000}
+
+# Vérification et instructions
+echo "Usage: sh serve_local_files.sh ROOT_DIR WILDCARD PORT"
 echo "This script scans INPUT_DIR directory with WILDCARD filter [all files by default],"
-echo "generates OUTPUT_FILE [files.txt by default] with a file list,"
-echo "starts web server on the port PORT [3000 by default] that serves files from INPUT_DIR with CORS enabled"
+echo "ROOT_DIR : Root Directory containing sub-directories with images"
+echo "WILDCARD : Filters pour les types de fichiers à servir [default = *]"
+echo "Starts web server on the port PORT [3000 by default] that serves files from INPUT_DIR with CORS enabled"
+echo "Generates output files"
 echo
 
-echo "Scanning ${INPUT_DIR} ..."
-FIND_CMD="find ${INPUT_DIR} -type f"
-if [ -z "$WILDCARD" ]; then
-  echo "Files wildcard is not set. Serve all files in ${INPUT_DIR}..."
-else
-  FIND_CMD="${FIND_CMD} -name ${WILDCARD}"
-fi
+# Loop through sub-directories
+for SUB_DIR in $ROOT_DIR/*; do
+  if [ -d "$SUB_DIR" ]; then
+    # Get sub-directory name and prepare txt file name
+    SUB_DIR_NAME=$(basename $SUB_DIR)
+    OUTPUT_FILE="${SUB_DIR_NAME}.txt"
 
-echo "Replacing ${INPUT_DIR} to http://localhost:${PORT} ..."
-INPUT_DIR_ESCAPED=$(printf '%s\n' "$INPUT_DIR" | sed -e 's/[\/&]/\\&/g')
-eval $FIND_CMD | sed "/${INPUT_DIR_ESCAPED}/s//http:\/\/localhost:${PORT}/" > $OUTPUT_FILE
+    echo "Scan sub-directory: ${SUB_DIR_NAME} ..."
 
-green=`tput setaf 2`
-reset=`tput sgr0`
-echo "${green}File list stored in '${OUTPUT_FILE}'. Now import it directly from Label Studio UI${reset}"
+    # Find image files and create txt file
+    FIND_CMD="find ${SUB_DIR} -type f -name ${WILDCARD}"
+    INPUT_DIR_ESCAPED=$(printf '%s\n' "$SUB_DIR" | sed -e 's/[\/&]/\\&/g')
+    eval $FIND_CMD | sed "s|${INPUT_DIR_ESCAPED}|http://localhost:${PORT}/${SUB_DIR_NAME}|g" > $OUTPUT_FILE
 
-echo "Running web server on the port ${PORT} with CORS enabled"
-cd $INPUT_DIR
+    echo "Created txt file : ${OUTPUT_FILE}"
+  fi
+done
+
+# Démarrage du serveur web
+echo "Run web server on port ${PORT} with CORS enabled"
+cd $ROOT_DIR
 http-server -p $PORT --cors
